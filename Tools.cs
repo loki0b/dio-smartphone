388918d42cd7a15677bd7727ffc056c8dcfd7cff
loki0b <riqc0nt4ct@hotmail.com>
2023-12-31 15:20:45 -0300

    add Tools class

---
+++ Tools.cs
@@ -0,0 +1,19 @@
+public static class Tools
+{
+  public static string GenerateCellNumber()
+  {
+    Random rnd = new Random();
+    string cellNumber = rnd.Next(87654321, 99999999).ToString();
+
+    return cellNumber;
+  }
+
+  public static string GenerateImei()
+  {
+    Random rnd = new Random();
+    string cellImei = rnd.Next(1234567, 9999999).ToString() + rnd.Next(12345678, 99999999).ToString();
+
+    return cellImei;
+
+  }
+}
