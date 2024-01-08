39289b914701a10a74c2b32da459c0e0e5a34126
loki0b <riqc0nt4ct@hotmail.com>
2024-01-08 20:50:54 -0300

    update

--- Program.cs
+++ Program.cs
@@ -1,13 +1,1 @@
-﻿using Models.Phones;
-
-Iphone phone = new Iphone(5);
-phone.InstallApp("Good", 0.5);
-phone.InstallApp("music", 4.5);
-phone.ListApps();
-Console.WriteLine(phone.AvailableMemory);
-phone.UninstallApp("music", 4.5);
-Console.WriteLine(phone.AvailableMemory);
-phone.InstallApp("Discord", 2);
-Console.WriteLine(phone.AvailableMemory);
-phone.UninstallApp("Discord", 2);
-Console.WriteLine(phone.AvailableMemory);
+﻿// TODO: emulate a smartphone
